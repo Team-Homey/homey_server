1a6a9ce429db6db26a5d77b1bb5c511bf35d0dc3
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-02-28 19:56:57 +0900

    register dtos 추가

--- src/main/java/com/homey/homeyserver/dto/RegisterResponse.java
+++ src/main/java/com/homey/homeyserver/dto/RegisterResponse.java
@@ -2,8 +2,11 @@ package com.homey.homeyserver.dto;
 
 
 import lombok.Builder;
+import lombok.Getter;
 
+//주석주석주~
 @Builder
+@Getter
 public class RegisterResponse {
     private String accessToken;
 }
